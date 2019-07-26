5ec18d493485e11ac1a1e7449f8d02500dddb687
Lucas Torres Mezzari <lucas.mezzari1@gmail.com>
2019-07-26 19:40:01 -0300

    Initial Commit

---
+++ app/src/main/java/mezzari/torres/lucas/livedata_binding/MainActivity.java
@@ -0,0 +1,14 @@
+package mezzari.torres.lucas.livedata_binding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
